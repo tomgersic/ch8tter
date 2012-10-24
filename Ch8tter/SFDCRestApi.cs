58feeba8a886f49f836bd66e86cd8bd7aebab48a
Tom Gersic <tom@gersic.com>
2012-10-23 19:17:54 -0700

    beginning on REST wrapper

---
+++ Ch8tter/SFDCRestApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8tter
+{
+    public class SFDCRestApi
+    {
+        public String Request(String method, String path)
+        {
+
+
+            return "";
+        }
+    }
+}
---
+++ Ch8tter/SFDCSession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8tter
+{
+    public sealed class SFDCSession
+    {
+        private static volatile SFDCSession instance;
+        private static object syncRoot = new Object();
+
+        public String AccessToken = "00Dd0000000dbdj!AR4AQNsQE0Xg3rsgpOmaeSPDjYWFdAHejSRy2xw6uGlwxlEu9ZzLxdxiOz3yMkpjSeKIV9Z3Ur4Dxgv1AIK24rjtiAjkO4tL";
+        public String InstanceUrl = "https://na14.salesforce.com/";
+        public String ApiVersion = "v26.0";      
+
+        private SFDCSession() { }
+
+        public static SFDCSession Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock(syncRoot)
+                    {
+                        if(instance == null)
+                        {
+                            instance = new SFDCSession();
+                        }
+                }
+                return instance;
+            }
+        }
+    }
+}
